60f9ab0b7037e4a784b6945cad7b36784f4c1370
Jonathon M. Abbott <jonathona@everydayhero.com.au>
2016-09-01 12:07:31 +1000

    Reset the all the connections if too many errors
    On ruby 1.8 we're stuck with bunny 1.x and it has some trouble
    recovering from graceful shutdowns. This tracks bunny errors and once
    we've seen enough we drain all the channel pools and brute force
    reconnect to RabbitMQ.

--- lib/queight/channel_pool.rb
+++ lib/queight/channel_pool.rb
@@ -1,6 +1,7 @@
 require "bunny"
 require "hot_tub"
 require "queight/channel_wrapper"
+require "monitor"
 
 module Queight
   class ChannelPool
@@ -9,19 +10,27 @@ module Queight
     def initialize(options)
       @options = options
       @wrappers = []
+      @error_count = 0
+      @lock = Monitor.new
     end
 
     def with_channel
-      channel_pool.run { |channel| yield(channel) }
+      tracking_bunny_errors do
+        channel_pool.run { |channel| yield(channel) }
+      end
     end
 
     def with_transactional_channel
-      transactional_channel_pool.run { |channel| yield(channel) }
+      tracking_bunny_errors do
+        transactional_channel_pool.run { |channel| yield(channel) }
+      end
     end
 
     def with_subscribe_channel(prefetch)
-      channel = create_channel(prefetch)
-      yield(channel)
+      tracking_bunny_errors do
+        channel = create_channel(prefetch)
+        yield(channel)
+      end
     ensure
       channel.close
     end
@@ -44,11 +53,11 @@ module Queight
     end
 
     def transactional_channel_pool
-      @tx_channel_pool ||= HotTub::Pool.new(pool_options) { build_wrapper }
+      @tx_channel_pool ||= new_pool("pool:tx_channels")
     end
 
     def channel_pool
-      @channel_pool ||= HotTub::Pool.new(pool_options) { build_wrapper }
+      @channel_pool ||= new_pool("pool:channels")
     end
 
     def build_wrapper
@@ -64,6 +73,10 @@ module Queight
       )
     end
 
+    def new_pool(name)
+      HotTub::Pool.new(pool_options.merge(:name => name)) { build_wrapper }
+    end
+
     def pool_options
       {
         :close => :close,
@@ -74,6 +87,41 @@ module Queight
 
     def pool_size
       @options.fetch(:size, DEFAULT_POOL_SIZE)
+    end
+
+    def tracking_bunny_errors
+      val = yield
+
+      no_bunny_error
+
+      return val
+    rescue Bunny::Exception => e
+      bunny_error
+      raise e
+    end
+
+    MAXIMUM_BUNNY_ERRORS = 3
+
+    def bunny_error
+      @error_count += 1
+      synch { reconnect! if @error_count > MAXIMUM_BUNNY_ERRORS }
+    end
+
+    def no_bunny_error
+      @error_count = 0
+    end
+
+    def synch
+      @lock.synchronize { yield }
+    end
+
+    def reconnect!
+      channel_pool.drain!
+      transactional_channel_pool.drain!
+      @conn.close if @conn
+      @conn = nil
+      @wrappers = []
+      @error_count = 0
     end
   end
 end
